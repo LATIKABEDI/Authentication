dd9ee84acbaf050702f3850a387f55454277f194
Latika BEDI <latika.bedi@sap.com>
2024-08-19 00:12:44 +0530

    Server setup & basic route setup

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,52 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connect from "./src/db/connect.js";
+import cookieParser from "cookie-parser";
+import fs from "node:fs";
+import { error } from "node:console";
+
+dotenv.config();
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+//middleware
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+//routes
+const routeFiles = fs.readdirSync("./src/routes");
+
+routeFiles.forEach((file) => {
+  import(`./src/routes/${file}`)
+    .then((route) => {
+      app.use("/api/v1", route.default);
+    })
+    .catch((error) => {
+      console.log("Failed to fetch the routes.......", error);
+    });
+});
+
 const server = async () => {
-  console.log("Server is running.");
+  try {
+    await connect();
+
+    app.listen(port, () => {
+      console.log(`Server is running at port ${port}`);
+    });
+  } catch (error) {
+    console.log("Failed to start the server......", error.message);
+    process.exit(1);
+  }
 };
 
 server();
